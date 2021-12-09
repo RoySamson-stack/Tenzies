099411f0fa7c82425cdb7b798870d85a6f3e5dd6
RoySamson-stack <rsam9009@gmail.com>
2021-12-09 22:17:16 +0300

    adding a map array for the dice components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import Die from "./Die";
 
 export default function App(){
+  
+ function allNewDice(){
+   const newDice = [];
+  for (let i = 0; i < 5; i++) {
+    newDice.push(Math.floor(Math.random()  * 6) + 1);
+  }
+  return newDice;
+  }
+  console.log(allNewDice)
   return(
-    <main></main>
+    <main>
+      <div className="dice-container">
+        <Die value="1"/>
+        <Die value="2"/> 
+        <Die value="3"/> 
+        <Die value="4"/> 
+        <Die value="5"/>
+        <Die value="6"/>
+        <Die value="1"/>
+        <Die value="1"/>
+        <Die value="1"/>
+        <Die value="1"/>
+      </div>
+    </main>
   )
 }
