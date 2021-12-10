10633111910969dff13eed281d6c414590322cae
RoySamson-stack <rsam9009@gmail.com>
2021-12-10 22:48:26 +0300

    refactoring the code for the dice for a object to array error

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
-import React from "react";
-import Die from "./Die";
-import {nanoid} from "nanoid";
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
 
+export default function App() {
+/**
+ * Challenge: Add conditional styling to the Die component
+ * so that if it's held (isHeld === true), its background color
+ * changes to a light green (#59E391)
+ * 
+ * Remember: currently the Die component has no way of knowing
+ * if it's "held" or not.
+ */
 
-export default function App(){
-  
-  const [dice, setDice] = React.useState(allNewDice);
-
- function allNewDice(){
-   const newDice = [];
-  for (let i = 0; i < 10; i++) {
-    newDice.push({
-      value: Math.ceil(Math.random() * 6),
-       isHeld: false,
-       id: nanoid()
-      })
-  }
-  return newDice;
-  }
-  const diceElements = dice.map(die => <Die value={die} />);
-
-   return(
-    //map over the array of dice and return a Die component for each one  
-    <main>
-      <div className="dice-container">
-        {diceElements}
-      </div>
-      <button className="rolldice" onClick={() => setDice(allNewDice())}>Roll Dice</button>
-    </main>
-  )
+    const [dice, setDice] = React.useState(allNewDice())
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false,
+                id: nanoid()
+            })
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        setDice(allNewDice())
+    }
+    
+    const diceElements = dice.map(die => (
+        <Die key={die.id} value={die.value} />
+    ))
+    
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+        </main>
+    )
 }
