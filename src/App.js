5ec9ce6ab56d2c3522f2f31055a95142831a40f8
RoySamson-stack <rsam9009@gmail.com>
2021-12-12 20:19:15 +0300

    reducing repetition of the code for the program

--- src/App.js
+++ src/App.js
@@ -7,23 +7,32 @@ export default function App() {
 
     const [dice, setDice] = React.useState(allNewDice())
     
+   
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+    function rollDice() {
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ? 
+                die :
+                generateNewDie()
+        }))
+    }
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push({
-                value: Math.ceil(Math.random() * 6), 
-                isHeld: false,
-                id: nanoid()
-            })
+            newDice.push(generateNewDie() )
         }
         return newDice
     }
-    
-    function rollDice() {
-        setDice(allNewDice())
-    }
     function holdDie(id) {
-        console.log(id)
+      setDice(oldDice => oldDice.map(die => {
+          return die.id === id ? {...die, isHeld: !die.isHeld} : die
+      }))
     }
     const diceElements = dice.map(die => (
         <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDie(die.id)} />
